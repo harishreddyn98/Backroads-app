35c722489c9ac9a3798c8e1c609a085426517c5d
Harish <harishreddyn98@gmail.com>
2024-03-06 15:44:18 +0530

    package-json changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,48 @@
-
-import { PageLinks,socialLinks } from '../data';
-import logo from '../images/logo.svg'
+import { PageLinks, socialLinks } from "../data";
+import logo from "../images/logo.svg";
 
 const Navbar = () => {
   return (
-      <nav className="navbar">
-        <div className="nav-center">
-          <div classNameName="nav-header">
-            <img
-              src={logo}
-              classNameName="nav-logo"
-              alt="backroads"
-            />
-            <button type="button" classNameName="nav-toggle" id="nav-toggle">
-              <i className="fas fa-bars"></i>
-            </button>
-          </div>
-          {/* <!-- left this comment on purpose --> */}
-          <ul className="nav-links" id="nav-links">
-            {PageLinks.map((link) => {
-              return (
-                <li id={link.id}>
-                  <a href={link.href} className="nav-link">
-                    {link.text}
-                  </a>
-                </li>
-              );
-            })
-            }
-          </ul>
-
-          <ul className="nav-icons">
- {
-  socialLinks.map((link) => {
-    return (
-      <li key= {link.id}>
-        <a href={link.href}target="_blank" className="nav-icon">
-          <i className={link.icon}></i>
-        </a>
-      </li>
-    );
-  })
- }
-          </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div classNameName="nav-header">
+          <img src={logo} classNameName="nav-logo" alt="backroads" />
+          <button type="button" classNameName="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
         </div>
-      </nav>
-  );
-}
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          {PageLinks.map((link) => {
+            return (
+              <li id={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
 
-export default Navbar
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a
+                  href={link.href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={link.icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
